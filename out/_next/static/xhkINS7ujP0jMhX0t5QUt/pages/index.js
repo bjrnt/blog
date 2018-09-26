a81d57a9b591d1dcd7f2e35721f89a1d5d486d09
Björn Tegelund <b.tegelund@gmail.com>
2018-09-26 12:19:40 +0900

    Add build

---
+++ out/_next/static/xhkINS7ujP0jMhX0t5QUt/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{208:function(n,e,t){__NEXT_REGISTER_PAGE("/",function(){return n.exports=t(209),{page:n.exports.default}})},209:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Welcome to next.js!")}}},[[208,1,0]]]);
